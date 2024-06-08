95b509d58590d11f9e74ff6ea9a57e0dbfb02526
Izkbeae <168627633+Izkbeae@users.noreply.github.com>
2024-06-08 10:33:33 +0900

    Add files via upload

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,15 @@
+<!-- complete.php -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>完了画面</title>
+</head>
+<body>
+    <h1>登録が完了しました</h1>
+    <p>社員情報がデータベースに保存されました。</p>
+    <form action="form.php" method="get">
+        <button type="submit">戻る</button>
+    </form>
+</body>
+</html>
